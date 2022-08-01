ad18902b4fc4dceffd094d5dd2aed2023003b1f9
IDONGO <63292417+IDONGO@users.noreply.github.com>
2022-08-01 11:54:25 +0700

    Update delete.py

--- bot/modules/delete.py
+++ bot/modules/delete.py
@@ -1,5 +1,4 @@
 from threading import Thread
-from telegram import Update
 from telegram.ext import CommandHandler
 
 from bot import dispatcher, LOGGER
@@ -11,12 +10,11 @@ from bot.helper.ext_utils.bot_utils import is_gdrive_link
 
 
 def deletefile(update, context):
-    args = update.message.text.split(" ", maxsplit=1)
     reply_to = update.message.reply_to_message
-    if len(args) > 1:
-        link = args[1]
-    elif reply_to is not None:
-        link = reply_to.text
+    if len(context.args) == 1:
+        link = context.args[0]
+    elif reply_to:
+        link = reply_to.text.split(maxsplit=1)[0].strip()
     else:
         link = ''
     if is_gdrive_link(link):
